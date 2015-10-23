a8b862d54ca9eeee169cbeb1a48fb1f8a4b45203
marquinhe <salinas@cmu.edu>
2015-10-23 12:05:08 +1030

    initial time difference calculation function: without timezones

--- client/helpers/stringHelpers.js
+++ client/helpers/stringHelpers.js
@@ -2,3 +2,10 @@ Template.registerHelper('truncate', function(string, length) {
   var cleanString = _(string).stripTags();
   return _(cleanString).truncate(length);
 });
+
+
+Template.registerHelper('substractDate', function(date1, date2) {
+	
+	var ms = moment(date2).diff(moment(date1),'days');
+  	return ms;
+});
