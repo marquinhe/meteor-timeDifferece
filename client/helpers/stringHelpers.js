35158fe6da540eb609f2d9c05bccad95bdf11c4f
marquinhe <salinas@cmu.edu>
2015-10-23 15:51:29 +1030

    Timezone support

--- client/helpers/stringHelpers.js
+++ client/helpers/stringHelpers.js
@@ -4,8 +4,36 @@ Template.registerHelper('truncate', function(string, length) {
 });
 
 
-Template.registerHelper('substractDate', function(date1, date2) {
-	
-	var ms = moment(date2).diff(moment(date1),'days');
-  	return ms;
+Template.registerHelper('substractDate', function(date1, timezone1, date2, timezone2, calculationType) {
+
+    var dateStart = moment.tz(date1, timezone1);
+    var dateEnd = moment.tz(date2, timezone2);
+
+    /*debugging
+    console.log("---");
+    console.log("date:" +cleanDate(date1));
+    console.log("date:" +cleanDate(date2));
+    console.log("DateStart:" + dateEnd.format() + " tz:"+timezone1);
+    console.log("DateEnd:" + dateStart.format() + " tz:"+timezone2);
+
+    var sameZoneDate = dateEnd.clone().tz(timezone1);
+
+	console.log("sameEnd:" + sameZoneDate.format());
+	console.log("---");
+	**/
+
+	var difference = moment(dateEnd).diff(moment(dateStart), calculationType);
+  	return difference;
+});
+
+
+Template.registerHelper('when', function(created) {
+
+   return moment(created).fromNow();
+});
+
+
+Template.registerHelper('formatDate', function(created) {
+
+   return moment(created).format('LL');
 });
